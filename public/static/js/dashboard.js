e136aa74fe5109994498736b74f73d0870c9cf1a
yanske1 <yansongke1@gmail.com>
2017-02-05 10:06:37 -0500

    firebase implementation

--- public/static/js/dashboard.js
+++ public/static/js/dashboard.js
@@ -1,44 +1,62 @@
-var app = angular.module('myApp', []);
-
-app.controller('myCtrl', function($scope) {
-	$scope.circuits = ["Chapter 13 Problem Set", "Memes"];
-	$scope.materials = ["Study Midterms", "Tutorial Questions"];
-	$scope.calculus = ["PSet 4"];
-	
-	$scope.cirAbout = false;
-	$scope.div1 = 0;
-	$scope.div2 = 0;
+(function() {
+  // Initialize Firebase
 
 
-	$scope.matAbout = false;
-	$scope.calAbout = false;
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBrt9LbBh44FUvc8_Ndpj3t36uiHZhwN6o",
+    authDomain: "batch-3fd9b.firebaseapp.com",
+    databaseURL: "https://batch-3fd9b.firebaseio.com",
+    storageBucket: "batch-3fd9b.appspot.com",
+    messagingSenderId: "727416862799"
+  };
+  firebase.initializeApp(config);
 
-	$scope.aboutHeight = 100;
 
-	$scope.addCircuits = function()
-	{
-		$scope.circuits.push($scope.addC);
-		$scope.addC = null;
-	}
-	$scope.addMaterials = function()
-	{
-		$scope.materials.push($scope.addM);
-		$scope.addM = null;
-	}
-	$scope.addCalculus = function()
-	{
-		$scope.calculus.push($scope.addCal);
-		$scope.addCal = null;
-	}
+angular
+    .module('myApp', ['firebase',])
+    .controller('MyCtrl', function($scope, $firebaseArray){
+        var rootRef = firebase.database().ref().child('tasks');
+        var cal = rootRef.child(0);
+        $scope.calculus= $firebaseArray(cal);
 
-	$scope.removeCir = function (x) {
-        $scope.circuits.splice(x, 1);
-    }
-    $scope.removeMat = function (x) {
-        $scope.materials.splice(x, 1);
-    }
-    $scope.removeCal = function (x) {
-        $scope.calculus.splice(x, 1);
-    }
+        $scope.circuits = ["Chapter 13 Problem Set", "Memes"];
+		$scope.materials = ["Study Midterms", "Tutorial Questions"];
 
-});
+		
+		$scope.cirAbout = false;
+		$scope.div1 = 0;
+		$scope.div2 = 0;
+
+		$scope.matAbout = false;
+		$scope.calAbout = false;
+
+		$scope.aboutHeight = 100;
+
+		$scope.addCircuits = function()
+		{
+			$scope.circuits.push($scope.addC);
+			$scope.addC = null;
+		}
+		$scope.addMaterials = function()
+		{
+			$scope.materials.push($scope.addM);
+			$scope.addM = null;
+		}
+		$scope.addCalculus = function()
+		{
+			$scope.calculus.push($scope.addCal);
+			$scope.addCal = null;
+		}
+
+		$scope.removeCir = function (x) {
+	        $scope.circuits.splice(x, 1);
+	    }
+	    $scope.removeMat = function (x) {
+	        $scope.materials.splice(x, 1);
+	    }
+	    $scope.removeCal = function (x) {
+	        $scope.calculus.splice(x, 1);
+	    }
+    });
+}());
